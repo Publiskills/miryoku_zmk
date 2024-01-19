22266a57df15ea8e2be1ead5b3e341a8ddd4048e
Fabien Wirig <fabien.wirig@gmail.com>
2024-01-19 02:33:07 +0100

    Fix button layer

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,6 +3,8 @@
 
 #include "locales/keys_fr.h"
 
+#define U_FR_UND &kp LC(FR_Z)
+
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
      K00, K01, K02, K03, K04,      K05, K06, K07, K08, K09, \
      K10, K11, K12, K13, K14,      K15, K16, K17, K18, K19, \
@@ -28,3 +30,9 @@ U_NP,                   U_NP,              U_LT(U_MEDIA, ESC),  U_LT(U_NAV, SPAC
 U_NP,              U_NP,              &kp ESC,           &kp SPACE,         &kp TAB,           &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_EXTRA MIRYOKU_LAYER_BASE
+
+#define MIRYOKU_LAYER_BUTTON \
+U_FR_UND,          U_CUT,             U_CPY,             U_PST,             U_RDO,          U_RDO,          U_PST,             U_CPY,             U_CUT,             U_FR_UND,             \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,           U_NU,           &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+U_FR_UND,          U_CUT,             U_CPY,             U_PST,             U_RDO,          U_RDO,          U_PST,             U_CPY,             U_CUT,             U_FR_UND,             \
+U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,         U_BTN2,         U_BTN1,            U_BTN3,            U_NP,              U_NP
