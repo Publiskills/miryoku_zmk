1eba4648a215dd3f8721129acb68f363d43f79e7
Fabien Wirig <fabien.wirig@gmail.com>
2024-01-20 01:01:11 +0100

    fix nav and num

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -4,10 +4,10 @@
 #include "locales/keys_fr.h"
 
 
-#define HRML(k1,k2,k3,k4) U_MT(LGUI, k1),U_MT(LALT, k2),U_MT(LCTRL, k3),U_MT(LSHFT, k4)
-#define HRMR(k1,k2,k3,k4) U_MT(LSHFT, k1),U_MT(LCTRL, k2),U_MT(LALT, k3),U_MT(LGUI, k4)
-//#define HRML(k1,k2,k3,k4) &kp k1, &kp k2, &kp k3, &kp k4
-//#define HRMR(k1,k2,k3,k4) &kp k1, &kp k2, &kp k3, &kp k4
+//#define HRML(k1,k2,k3,k4) U_MT(LGUI, k1),U_MT(LALT, k2),U_MT(LCTRL, k3),U_MT(LSHFT, k4)
+//#define HRMR(k1,k2,k3,k4) U_MT(LSHFT, k1),U_MT(LCTRL, k2),U_MT(LALT, k3),U_MT(LGUI, k4)
+#define HRML(k1,k2,k3,k4) &kp k1, &kp k2, &kp k3, &kp k4
+#define HRMR(k1,k2,k3,k4) &kp k1, &kp k2, &kp k3, &kp k4
 
 #define U_FR_UND &kp LC(FR_Z)
 
@@ -42,3 +42,15 @@ U_FR_UND,          U_CUT,             U_CPY,             U_PST,             U_RD
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NU,           U_NU,           &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 U_FR_UND,          U_CUT,             U_CPY,             U_PST,             U_RDO,          U_RDO,          U_PST,             U_CPY,             U_CUT,             U_FR_UND,             \
 U_NP,              U_NP,              U_BTN3,            U_BTN1,            U_BTN2,         U_BTN2,         U_BTN1,            U_BTN3,            U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NAV \
+U_NU,              &u_to_U_TAP,       &u_to_U_EXTRA,     &u_to_U_BASE,      U_NA,              &kp INS,           &kp HOME,          &kp UP,            &kp END,           &kp PG_UP,         \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         U_NA,              &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp RIGHT,         &kp PG_DN,         \
+U_NA,              &kp RALT,          &u_to_U_NUM,       &u_to_U_NAV,       U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_FR_UND,          \
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
+#define MIRYOKU_LAYER_NUM \
+&kp FR_LBKT,       &kp FR_N7,         &kp FR_N8,         &kp FR_N9,         &kp FR_RBKT,       U_NA,              &u_to_U_BASE,      &u_to_U_EXTRA,     &u_to_U_TAP,       U_NU,            \
+&kp FR_SEMI,       &kp FR_N4,         &kp FR_N5,         &kp FR_N6,         &kp FR_EQUAL,      U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
+&kp FR_GRAVE,      &kp FR_N1,         &kp FR_N2,         &kp FR_N3,         &kp FR_BSLH,       U_NA,              &u_to_U_NUM,       &u_to_U_NAV,       &kp RALT,          U_NA,              \
+U_NP,              U_NP,              &kp FR_DOT,        &kp FR_N0,         &kp FR_MINUS,      U_NA,              U_NA,              U_NA,              U_NP,              U_NP
