de1f123444a4d3e4b28ed926f639f4321a2f29de
Fabien Wirig <fabien.wirig@gmail.com>
2026-03-22 10:57:25 +0100

    qwe

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -21,15 +21,15 @@
 #define U_FR_UND &kp LC(FR_Z)
 
 #define MIRYOKU_LAYOUTMAPPING_CORNE( \
-     K00, K01, K02, K03, K04,                                                   K05, K06, K07, K08, K09, \
-     K10, K11, K12, K13, K14,                                                   K15, K16, K17, K18, K19, \
-     K20, K21, K22, K23, K24,                                                   K25, K26, K27, K28, K29, \
-     N30, N31, K32, K33, K34,                                                   K35, K36, K37, N38, N39 \
+     K00, K01, K02, K03, K04,                                                          K05, K06, K07, K08, K09, \
+     K10, K11, K12, K13, K14,                                                          K15, K16, K17, K18, K19, \
+     K20, K21, K22, K23, K24,                                                          K25, K26, K27, K28, K29, \
+     N30, N31, K32, K33, K34,                                                          K35, K36, K37, N38, N39 \
 ) \
-&kp TAB    K00  K01  K02  K03  K04                        &kp FR_U              K05  K06  K07  K08  K09  &kp BSPC \
-&kp LSHFT  K10  K11  K12  K13  K14              &kp FR_L  &kp FR_C  &kp FR_R    K15  K16  K17  K18  K19  U_LT_BIND(U_ACC, FR_SQT_S)\
-&kp LCTRL  K20  K21  K22  K23  K24                        &kp FR_D              K25  K26  K27  K28  K29  &kp RET \
-                     K32  K33  K34    &kp FR_Z                                  K35  K36  K37
+&kp TAB    K00  K01  K02  K03  K04                               &kp FR_U              K05  K06  K07  K08  K09  &kp BSPC \
+&kp LSHFT  K10  K11  K12  K13  K14                     &kp FR_L  &kp FR_C  &kp FR_R    K15  K16  K17  K18  K19  U_LT_BIND(U_ACC, FR_SQT_S)\
+&kp LCTRL  K20  K21  K22  K23  K24    &kp FR_Z                   &kp FR_D              K25  K26  K27  K28  K29  &kp RET \
+                     K32  K33  K34                                                     K35  K36  K37
 
 
 #define MIRYOKU_LAYER_BASE \
