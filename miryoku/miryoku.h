14bc230b646aa7d1a40cbf29273a670230569dfa
Fabien Wirig <fabien.wirig@gmail.com>
2026-03-25 01:12:52 +0100

    nice !!!!

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -19,14 +19,6 @@
   }; \
 };
 
-#if defined (EYELASH_CORNE)
-/ {
-    vbatt: vbatt {
-        compatible = "zmk,battery-nrf-vddh";
-    };
-};
-#endif
-
 #define U_NP &none // key is not present
 #define U_NA &none // present but not available for use
 #define U_NU &none // available but not used
